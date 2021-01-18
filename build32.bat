f1ca99fbe4e4ed5f9d32e491e3ba048f776aca68
Xxsource98 <36642285+Xxsource98@users.noreply.github.com>
2021-01-18 21:33:45 +0100

    Update build32.bat

--- build32.bat
+++ build32.bat
@@ -1,5 +1,9 @@
 @ECHO OFF
-cd Vendor/SFML
+cd Vendor
+git clone https://github.com/ocornut/imgui.git
+git clone https://github.com/eliasdaler/imgui-sfml.git
+git clone https://github.com/SFML/SFML.git
+cd SFML
 cmake -G "Visual Studio 16 2019" -A Win32 -S . -B "build"
 cmake --build build --config Release
 cmake --build build --config Debug
